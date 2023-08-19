93b4c4514363cc6c3bec0943e259d96503f1edbe
GiangDT91 <142680039+GiangDT91@users.noreply.github.com>
2023-08-20 02:09:30 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -32,7 +32,6 @@ urls=(
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
     # Commented out because it whitelists sites including doubleclick.net and ad.atdmt.com
     # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
-
 )
 
 # loop through the urls and download each file with curl
